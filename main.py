629bd97b91df1c6eb3a992ed23fd7994a2ddc240
coolunderstanding37 <rael.brown96@gmail.com>
2024-10-22 19:12:58 -0400

    added character count function

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 def main():
     
+    # retrieving the book file
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     
+    # counting words
     word_count = count_words(file_contents)
     print(f"The book contains {word_count} words.")
+
+    # lowercase
+    convert_lowercase = file_contents.lower()
+
+    # call char_count
+    total_char = char_count(file_contents)
+    print("Character count: ")
+    print(f"{total_char}")
        
 def count_words(text):
     words = text.split()
@@ -12,6 +22,17 @@ def count_words(text):
     for word in words:
         word_count += 1
     return word_count
+
+def char_count(text):
+    char_dict = {}
+    lowercase_char = text.lower()
+    for char in lowercase_char:
+        if char not in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
+
+    return char_dict
 
 if __name__ == "__main__":
     main()
